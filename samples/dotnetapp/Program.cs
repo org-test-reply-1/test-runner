c0d8f901f6ee646a4f9ed1e4b01e23e20310d066
mattiamilani <mattiamilani960@gmail.com>
2026-05-07 12:13:20 +0200

    Add error logging for testing in Program.cs

--- samples/dotnetapp/Program.cs
+++ samples/dotnetapp/Program.cs
@@ -97,3 +97,6 @@ static bool TryReadFirstLongFromPaths(string[] paths, out long limit, [NotNullWh
 
 // Utilizzo della nuova funzionalità
 WriteLine(GetRunTimestamp());
+
+
+          ERRORE TESTING
